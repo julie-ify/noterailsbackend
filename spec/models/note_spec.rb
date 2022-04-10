1722cc80d0bfdcfc228e8f926105ca1d46252d64
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 16:04:39 +0100

    Write note model test using shoulda matcher gem

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  let!(:user) { User.create(username: 'testuser', password: 'password', age: '20') }
-  let!(:note) { Note.create(title: '', body: 'This is note 1') }
 
   describe 'associations' do
     it { should belong_to(:user).class_name('User') }
@@ -10,7 +8,11 @@ RSpec.describe Note, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:body) }
 		it { should validate_length_of(:title).is_at_least(3) }
+		it { should validate_length_of(:title).is_at_most(50) }
+		
+    it { should validate_presence_of(:body) }
+		it { should validate_length_of(:body).is_at_least(3) }
+		it { should validate_length_of(:body).is_at_most(250) }
   end
 end
