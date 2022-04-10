2ff37ef71e5c018f22237442a5c6a130eade5503
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 14:37:54 +0100

    Rswag configuration and setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
+
+	has_many :notes
+
+	validates :username, presence: true, uniqueness: true, length: { minimum: 3,
+	too_short: "%{username} characters is the minimum allowed" }
+
+	validates :password, presence: true, length: { minimum: 3, maximun: 50,
+	too_long: "%{password} characters is the maximum allowed" }
 end
