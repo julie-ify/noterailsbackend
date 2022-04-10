1a8e415b0a6db300d4f5b7a6be2517e177c941b1
Juliana Ifionu <julieifionu@gmail.com>
2022-04-10 15:06:49 +0100

    Add root path for rswag Ui engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Ui::Engine => '/'
   mount Rswag::Api::Engine => '/api-docs'
   resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resource :users, only: [:create]
-  post "/login", to: "users#login"
+  post "login", to: "users#login"
   get "auto_login", to: "users#auto_login"
 end
